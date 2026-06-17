2f0d4188db0d10ad0005daf9445e4a5eaac545da
Aditya Kumar <adityakumar29f70@gmail.com>
2026-06-17 23:35:39 +0530

    built mcp client using langgraph and tried to add with  mathematical calculater (local mcp server)

--- 19_mcp_client_langGraph/Readme.md
+++ 19_mcp_client_langGraph/Readme.md
@@ -4,3 +4,13 @@
 - or mcp is a standard way to integrate tools with our llm application;
 
 ## Problem with tools
+
+- mcp is inhence version of tools (bz teditional tools connection had some issues)
+
+* it had tools access like web-search, calculator and get stock price
+* now i want to connect our chat to git-hub copilate so we can ask about our repos(bz we want to build that chatbot for our company develaper team)
+
+* langchain does have any pre-build github tool, we have to build that tool(custom tool)
+* build a tool where we can ask for how many pr i made on that rep
+
+## today's class we will try to build our own mcp client using langGraph
---
+++ 19_mcp_client_langGraph/mcp_backend.py
@@ -0,0 +1,202 @@
+import os
+import json
+import asyncio
+import aiosqlite
+from langchain_mcp_adapters.client import MultiServerMCPClient
+from dotenv import load_dotenv
+from langgraph.graph import StateGraph, add_messages, START, END
+from typing import Annotated, TypedDict
+from langchain_groq import ChatGroq
+from langchain_core.messages import BaseMessage, SystemMessage, AIMessage, HumanMessage
+from langgraph.prebuilt import ToolNode, tools_condition
+from langchain.tools import tool
+from langgraph.checkpoint.sqlite.aio import AsyncSqliteSaver
+import sqlite3
+
+
+
+load_dotenv()
+
+DB_PATH= os.path.join(os.path.dirname(__file__), 'chatbot.db')
+
+async def init_db(): # changed: add async
+    try:
+        # Changed: sqlite3.connect -> aiosqlite.connect
+        async with aiosqlite.connect(DB_PATH) as db:
+            await db.execute("""
+            CREATE TABLE IF NOT EXISTS expenses(
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                date TEXT NOT NULL,
+                amount TEXT NOT NULL,
+                category TEXT NOT NULL,
+                subcategory TEXT DEFAULT '',
+                note TEXT DEFAULT ''
+            )
+            """)
+            await db.commit()
+    except Exception as e:
+        print(f"Database initialization error: {e}")
+        raise
+
+SERVERS={
+    "math": {
+
+        'transport':'stdio',
+        "command": "C:\\Users\\Aditya Kumar\\AppData\\Roaming\\Python\\Python310\\Scripts\\uv.exe",
+        "args": [
+        "run",
+        "--python",
+        "3.12",
+        "--with",
+        "fastmcp",
+        "fastmcp",
+        "run",
+        "D:\\Users\\Aditya_Kumar\\mcp_math_server\\main.py"
+        ]
+    },
+}
+
+model= ChatGroq(
+    model="meta-llama/llama-4-scout-17b-16e-instruct",
+    api_key= os.getenv("GROQ_API_KEY")
+)
+
+
+
+async def main():
+    await init_db()
+
+    async with aiosqlite.connect(DB_PATH) as conn:
+        # for checkpointer
+        checkpointer= AsyncSqliteSaver(conn=conn)
+
+        client= MultiServerMCPClient(SERVERS)
+        # using this client we have to fetch all the tools present on local mcp server (math)
+        tools= await client.get_tools() # list of tools
+
+        model_with_tools= model.bind_tools(tools)
+
+
+
+        class ChatBotState(TypedDict):
+            messages: Annotated[list[BaseMessage], add_messages]
+
+
+        async def chat_handle(state:ChatBotState):
+            messages= [SystemMessage(content="You are question answering chat bot, and if you don't know about anything which is asked please tell that you don't know instead of telling irrelavent response"), *state['messages']]
+
+            ai_result= model_with_tools.invoke(messages)
+
+            return {'messages': [ai_result]}
+
+
+        tool_node= ToolNode(tools)
+
+        builder= StateGraph(ChatBotState)
+
+        builder.add_node('chat_handle', chat_handle)
+        builder.add_node('tools', tool_node)
+
+        builder.add_edge(START, 'chat_handle')
+        builder.add_conditional_edges('chat_handle', tools_condition)
+        builder.add_edge('tools', 'chat_handle')
+
+
+        graph= builder.compile(checkpointer=checkpointer)
+
+        config1= {'configurable':{'thread_id':1}}
+
+        user_query= input("Ask!: ")
+
+        result= await graph.ainvoke({'messages':[HumanMessage(content=user_query)]},config= config1)
+
+        print(f"User: {user_query}")
+        print(f"AI: {result['messages'][-1].content}")
+
+
+if __name__=='__main__':
+    asyncio.run(main())
+
+
+
+
+# # creating a database for checkpoint
+# conn= sqlite3.connect(database='./chatbot.db', check_same_thread=False) # so it will great that name database if there is not any database is created with that name 
+
+# # for checkpointer
+# checkpointer= SqliteSaver(conn=conn)
+
+
+# @tool()
+# def calculator(first_num:float, sec_num:float, operation:str) -> str:
+#     """
+#     Perform a basic arithmetic operation on two numebers.
+#     Supported operations: add , sub, mul, div
+#     """
+
+#     if operation=='add':
+#         result= first_num+ sec_num
+#     elif operation=='sub':
+#         result= first_num- sec_num
+#     elif operation=='mul':
+#         result= first_num* sec_num
+#     elif operation=='div':
+#         if sec_num==0:
+#             return {"error": "Denominator can't be zeor!"}
+#         result= first_num/sec_num
+#     else:
+#         return {"error":f"Unsupported operation '{operation}'"}
+#     return {"first_num":first_num, "second_num":sec_num, "operation":operation, "result":result}
+
+# tool_list= [calculator]
+
+
+
+
+
+# model= ChatGroq(
+#     model="meta-llama/llama-4-scout-17b-16e-instruct",
+#     api_key= os.getenv("GROQ_API_KEY")
+# )
+
+# model_with_tools= model.bind_tools(tool_list)
+
+
+# class ChatBotState(TypedDict):
+#     messages: Annotated[list[BaseMessage], add_messages]
+
+
+# def chat_handle(state:ChatBotState):
+#     messages= [SystemMessage(content="You are question answering chat bot, and if you don't know about anything which is asked please tell that you don't know instead of telling irrelavent response"), *state['messages']]
+
+#     ai_result= model_with_tools.invoke(messages)
+
+#     return {'messages': [ai_result]}
+
+
+# tool_node= ToolNode(tool_list)
+
+# builder= StateGraph(ChatBotState)
+
+# builder.add_node('chat_handle', chat_handle)
+# builder.add_node('tools', tool_node)
+
+# builder.add_edge(START, 'chat_handle')
+# builder.add_conditional_edges('chat_handle', tools_condition)
+# builder.add_edge('tools', 'chat_handle')
+
+
+# graph= builder.compile(checkpointer=checkpointer)
+
+# config1= {'configurable':{'thread_id':1}}
+
+# user_query= input("Ask!: ")
+
+# result= graph.invoke({'messages':[HumanMessage(content=user_query)]},config= config1)
+
+# print(f"User: {user_query}")
+# print(f"AI: {result['messages'][-1].content}")
+
+
+
+
